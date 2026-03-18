35365304e18ec24d2dbf3640c61bf60e485a2b98
Akash Bhosale <abhosale5010@gmail.com>
2026-03-18 13:39:03 +0530

    initial commit

---
+++ src/main/java/com/codenza/enotes/ENotesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.codenza.enotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENotesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENotesRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codenza/enotes/ENotesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenza.enotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
